ebbb4bb119eae31bbf8e67c7dbc308bc7f590bbb
ashtonfox20 <ashfox@bu.edu>
2024-04-28 21:11:54 -0400

    blurred images added to test file

--- blurImages.py
+++ blurImages.py
@@ -9,11 +9,15 @@ def blur_images(inputFolder, outputFolder): #outputFolder does not need to exist
     imgFiles = os.listdir(inputFolder)
     
     for file in imgFiles:
+        if file == ".DS_Store":
+            continue
+        blurredFileName = os.path.splitext(file)[0] + 'b' + os.path.splitext(file)[1]
+        
         imgPath = os.path.join(inputFolder, file)
         image = Image.open(imgPath)
         blurredImage = image.filter(ImageFilter.GaussianBlur(np.random.randint(3, 10)))
-        output_path = os.path.join(outputFolder, file)
+        output_path = os.path.join(outputFolder, blurredFileName)
         blurredImage.save(output_path)
         
     print("Finished blurring images.")
-blur_images("./images_to_blur", "./blurred_images")
+blur_images("./test", "./test-blurred")
